58eb966aa988851c5a0bfb9d6e148d7efe71aab2
Diogo Godoi <diogogoddoi@gmail.com>
2021-03-03 22:23:16 -0300

    Teste para estágio

---
+++ Index.js
@@ -0,0 +1,62 @@
+
+//Configurando express
+const express = require("express");
+const app = express();
+
+//Configurando body-parser
+const bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+//configurando api correios
+const Correios = require('node-correios');
+const correios = new Correios();
+
+
+//configurando template engine ejs
+app.set("view engine", "ejs");
+app.set("views");
+
+//configurando a pasta publica
+app.use(express.static("public"));
+
+
+//rota correios
+app.get("/Correios", (req, res) => {
+    var Valor = req.body.CEP
+    let mensagem =""
+  
+    //Objeto vazio para receber os dados da api
+    let result = {    
+      logradouro: '',
+      bairro: '',
+      localidade: '',
+      uf: '',
+    }
+
+    res.render("Correios.ejs", {Valor, result, mensagem})
+})
+
+
+//rota post para cep
+app.post("/Cep", (req,res)=>{
+  var Valor = req.body.CEP
+  let mensagem = "Cep Encontrado:"
+
+ //Função de consulta de cep da api 
+ correios.consultaCEP({ cep: Valor })
+.then(result => {
+  res.render('Correios.ejs', {result, mensagem})
+  console.log(result);
+})
+.catch(error => {
+ 
+  console.log(error)
+});
+
+  
+})
+
+//configuração porta http 3000
+app.listen(3000)
+
